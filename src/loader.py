6233abaac4d7fbdb9f3fcff719e10c227cebd22b
trusov13 <atrusov03@gmail.com>
2026-02-21 19:52:49 +0300

    Update loader.py

--- src/loader.py
+++ src/loader.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def load_patients():
-    return pd.read_csv('../data/patients.csv')
+    return pd.read_csv('data/patients.csv')
 
 def load_encounters():
-    return pd.read_csv('../data/encounters.csv')
+    return pd.read_csv('data/encounters.csv')
 
 if __name__ == "__main__":
     print(load_patients().head())
